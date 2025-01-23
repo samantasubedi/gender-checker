457325de6d894ed9c7e6083228fc94a87c51c503
samanta subedi <samantasubedi333@gmail.com>
2025-01-23 23:09:15 +0545

    js fixed

--- gender-checker.js
+++ gender-checker.js
@@ -1,12 +1,25 @@
 const fetchdata = () => {
-  fetch("https://api.genderize.io?name=pert")
+  const name = document.getElementById("name").value;
+
+  fetch(`https://api.genderize.io?name=${name}`)
     .then((response) => {
       if (!response.ok) {
-        throw new error("cannot fetch data");
+        throw new Error("cannot fetch data");
       } else {
         return response.json();
       }
     })
-    .then((data) => console.log(data))
+    .then((data) => {
+      // const { gender, probability, count } = data;//this extracts the data from the object ie data of gender,probability and count and stores that data creating the variable of same name.
+      // console.log(gender, probability, count);
+      // we can also store the data creating our own variables manually as below
+      const gender = data.gender;
+      const probability = data.probability;
+      const count = data.count;
+      console.log(gender, probability, count);
+      document.getElementById("gen").textContent = gender;
+      document.getElementById("prob").textContent = probability;
+      document.getElementById("num").textContent = count;
+    })
     .catch((error) => console.error(error));
 };
